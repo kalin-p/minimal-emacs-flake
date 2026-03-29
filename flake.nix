d6a2e09c067e93375feb5605a1b600b1bfee0686
Kalin Petkov <kalinpetkov314@gmail.com>
2026-03-30 01:13:10 +0200

    Compilation works
    Finally got native comp to work. Needed to set `LIBRARY_PATH` both during build
    and in the wrapper, to make sure Emacs can find the libgccjit and glibc
    libraries and invoke gcc.

--- flake.nix
+++ flake.nix
@@ -7,71 +7,113 @@
       flake = false;
     };
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    treesitter-grammars.url = "github:kalin-p/nix-treesitter/7f76f9f54407ae57c85655c2d09642e19810c52e";
+    treesitter-grammars.url = "github:kalin-p/nix-treesitter/39b550924b3a7f10885d8687a4a4b447976dfe3f";
   };
 
   outputs = { self, nixpkgs, emacs, treesitter-grammars }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = nixpkgs.legacyPackages.${system};
+      lib = pkgs.lib;
+
+      grammars = treesitter-grammars.packages.${system}.default;
+
+      libGccJitLibraryPaths = with pkgs; [
+        "${lib.getLib libgccjit}/lib/gcc"
+        "${lib.getLib stdenv.cc.libc}/lib"
+      ]
+      ++ lib.optionals (stdenv.cc ? cc.lib.libgcc) [
+        "${lib.getLib stdenv.cc.cc.lib.libgcc}/lib"
+      ];
+
     in
+      # versions of the stdenv GCC and libgccjit from nixpkgs must be the same
+      assert true -> (pkgs.stdenv.cc.cc.version == pkgs.libgccjit.version);
+      # TODO: look into whether a check should be put to determine if build
+      # platform must be able to execute host platform or whatever they are
+      # doing in the emacs package from nixpkgs
       {
         packages.${system}.default = pkgs.stdenv.mkDerivation {
-          pname = "kalins_emacs";
+          pname = "emacs";
           version = "30.2";
           src = emacs;
 
           nativeBuildInputs = with pkgs; [
-            autoconf
+            makeWrapper
             pkg-config
+            autoreconfHook
+            texinfo
             wrapGAppsHook3
-            # libgccjit
-            gcc
           ];
 
           buildInputs = with pkgs; [
+            gettext
             gnutls
-            texinfo
+            (lib.getDev harfbuzz)
+            jansson
+            ncurses
+            dbus
+            libselinux
+            gsettings-desktop-schemas
+            libgccjit
+            zlib
+            giflib
             gtk3
+            libjpeg
+            libpng
+            librsvg
+            libtiff
+            gdk-pixbuf
+            cairo
             glib
             sqlite
+            systemd
             tree-sitter
-            ncurses
-            cairo
           ];
 
-          # propagatedBuildInputs = [ treesitter-grammars.packages.${system}.default ];
+          propagatedUserEnvPkgs = [
+            pkgs.mailutils
+          ];
 
-          #               --with-native-compilation=aot\
-          configurePhase = ''
-            ./autogen.sh
+          # correctly lists all grammars in the appropriate naming convention.
+          # preConfigure = ''
+          #   ls -lah ${grammars}
+          # '';
 
-            echo ${treesitter-grammars.packages.${system}.default}
+          preFixupPhases = [ "addTreesitterLoc" ];
 
-            ./configure \
-              --with-mailutils\
-              --with-json\
-              --with-tree-sitter\
-              --with-pgtk\
-              --program-prefix=kalins_\
-              --prefix=$out
-          '';
 
-          buildPhase = ''
-            make -j8
-          '';
+          addTreesitterLoc = ''
+                gappsWrapperArgs+=(
+                  --set KALINS_TREESITTER_GRAMMAR_LOCAION ${grammars}
+                  --set LIBRARY_PATH ${lib.concatStringsSep ":" libGccJitLibraryPaths}
+               )
+            '';
 
-          installPhase = ''
-            make install
-          '';
+          configureFlags = [
 
-          postInstall = ''
-            # mkdir -p $out/lib/tree-sitter
-            echo marker_kalin_1
-            echo $out/lib/tree-sitter
-            ln -s ${treesitter-grammars.packages.${system}.default} $out/lib/tree-sitter
-            ls $out/lib/tree-sitter
-          '';
+            "--with-pgtk"
+            "--with-mailutils"
+            "--with-json"
+            "--with-tree-sitter"
+            "--with-native-compilation=aot"
+            # "--program-prefix=kalins_"
+            "--verbose"
+          ];
+
+          env = {
+            NATIVE_FULL_AOT = "1";
+            LIBRARY_PATH = lib.concatStringsSep ":" libGccJitLibraryPaths;
+          };
+
+          enableParallelBuilding = true;
+
+          # TODO: figure out what this does
+          installTargets = [
+            "tags"
+            "install"
+          ];
+
         };
       };
 }
